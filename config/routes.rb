69feb168a3b22c4a324857706cdb28ed5a4ff24f
goshin-56 <102846714+goshin->
2022-04-10 06:42:58 +0000

    [Add]4-16

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:show, :edit, :update]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   root :to => 'homes#top'
+  get 'about' => 'homes#about',as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
